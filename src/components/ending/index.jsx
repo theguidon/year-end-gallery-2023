bb755fa92d9fb4d90fb12ff09909ae972325571a
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2023-05-18 05:29:30 +0800

    add photos bylines

--- src/components/ending/index.jsx
+++ src/components/ending/index.jsx
@@ -11,6 +11,15 @@ function Ending() {
       <img className="title" src={title} alt="Year-End Gallery" />
 
       <p className="credits">
+        Photos by <strong>Miguel Abad</strong>, <strong>Stella Arenas</strong>,{" "}
+        <strong>Jhanine Caoile</strong>, <strong>Karl Dimaculangan</strong>,{" "}
+        <strong>Elly Kim</strong>, <strong>Soleil Nicolette</strong>,{" "}
+        <strong>Mikyla Reyes</strong>, <strong>Patrick Reyes-Santos</strong>,{" "}
+        <strong>Jillian C. Santos</strong>, <strong>Paulina Singh</strong>,{" "}
+        <strong>Daryl D. Sy</strong>, <strong>France Vicente</strong>, and{" "}
+        <strong>Vionna Villalon</strong>
+        <br />
+        <br />
         Designed by <strong>Emman Evangelista</strong> and{" "}
         <strong>Josh de Vera</strong>
         <br />
