78235946d0693c1236d11b6795a3259c4d5d725c
Waleed Lugod <lugodwaleed@gmail.com>
2023-05-18 00:59:38 +0800

    photos

--- src/components/timeline/index.jsx
+++ src/components/timeline/index.jsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import "./photos.css";
 import PhotosData from "./../../data/index.jsx";
 
 function Timeline() {
@@ -6,21 +7,12 @@ function Timeline() {
     <section id="timeline">
       <p className="message">For a better viewing experience, please view the gallery in full screen.</p>
 
-      <h1 className="year 2022">2022</h1>
+      <h1 className="year _2022">2022</h1>
 
-      <h1 className="year 2023">2023</h1>
+      <h1 className="year _2023">2023</h1>
 
       {Object.keys(PhotosData).map((slug) => {
-        const dimensions = PhotosData[slug].dimension.split(" ");
-        return (
-          <img
-            className={`anim${slug}`}
-            key={slug}
-            src={PhotosData[slug].img}
-            alt=""
-            style={{ aspectRatio: dimensions[0] / dimensions[2] }}
-          />
-        );
+        return <img className={`anim-${slug}`} key={slug} src={PhotosData[slug].img} alt="" />;
       })}
     </section>
   );
