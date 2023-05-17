f61d4ca943615b1a0100d3595ce01eea1f26e658
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2023-05-18 02:40:11 +0800

    finalize watermark

--- src/components/watermark/index.jsx
+++ src/components/watermark/index.jsx
@@ -1,12 +1,18 @@
 import "./index.css";
 
+import logo from "./../../images/watermark/logo.svg";
+import text from "./../../images/watermark/text.svg";
+
 function Watermark(props) {
   return (
     <div
       id="watermark"
       className={`${props.position} ${props.active ? "active" : ""}`}
     >
-      <h1>Watermark {props.byline}</h1>
+      <img src={logo} alt="" className="logo" />
+      <img src={text} alt="" className="text" />
+      <div className="line" />
+      <p className="byline">{props.byline}</p>
     </div>
   );
 }
