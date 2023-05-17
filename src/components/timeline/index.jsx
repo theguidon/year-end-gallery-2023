43697b71eeaadefebde7cb1e6e8dd2d9b7c538a0
Waleed Lugod <lugodwaleed@gmail.com>
2023-05-17 22:38:36 +0800

    added photos

--- src/components/timeline/index.jsx
+++ src/components/timeline/index.jsx
@@ -1,15 +1,27 @@
 import "./index.css";
+import PhotosData from "./../../data/index.jsx";
 
 function Timeline() {
   return (
     <section id="timeline">
-      <p className="message">
-        For a better viewing experience, please view the gallery in full screen.
-      </p>
+      <p className="message">For a better viewing experience, please view the gallery in full screen.</p>
 
       <h1 className="year 2022">2022</h1>
 
       <h1 className="year 2023">2023</h1>
+
+      {Object.keys(PhotosData).map((slug) => {
+        const dimensions = PhotosData[slug].dimension.split(" ");
+        return (
+          <img
+            className={`anim${slug}`}
+            key={slug}
+            src={PhotosData[slug].img}
+            alt=""
+            style={{ aspectRatio: dimensions[0] / dimensions[2] }}
+          />
+        );
+      })}
     </section>
   );
 }
